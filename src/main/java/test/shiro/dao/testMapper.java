c4efb34d9e267b27b092790c3e6333732fc31485
rxk <1806146700@qq.com>
2018-08-21 18:41:07 +0800

    完善mybatis的xml

---
+++ src/main/java/test/shiro/dao/testMapper.java
@@ -0,0 +1,12 @@
+package test.shiro.dao;
+
+/**
+ * ======================
+ * Created By User: RXK
+ * Date: 2018/8/21  18:01
+ * Version: V1.0
+ * Description:
+ * ======================
+ */
+public class testMapper {
+}
